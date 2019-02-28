3bfb2737837b58fa2cf519a3e7e7747ff5f6b3d8
jkirkwood <jamesk1187@gmail.com>
2019-02-28 00:19:52 +0000

    Move world directory before applying backup

--- ami/bundle/home/ubuntu/scripts/restore-backup.sh
+++ ami/bundle/home/ubuntu/scripts/restore-backup.sh
@@ -8,6 +8,10 @@ backup_name=$1
 region=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r ".region")
 bucket=$(aws ssm get-parameter --name "/minecraft-server/s3-bucket-name" --query "Parameter.Value" --output text --region "$region")
 
+# Remove current local world backup and move existing world folder
+rm /home/ubuntu/minecraft/world-backup || true
+mv /home/ubuntu/minecraft/world /home/ubuntu/minecraft/world-backup
+
 # Download backup from S3 and apply to minecraft directory
 aws s3 cp "s3://${bucket}/backups/${backup_name}" /tmp/world.tar.gz
 tar -C /home/ubuntu/minecraft -xzf /tmp/world.tar.gz
